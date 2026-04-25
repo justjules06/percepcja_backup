765b24c5d44d0e98cd20f0eae0e895ec63128e2c
Julia Koc <julkoc5@st.amu.edu.pl>
2026-04-25 22:54:10 +0200

    zamiana sciezki lokalnej na globalna

--- untitled_FINAL_NEWEST_lastrun.py
+++ untitled_FINAL_NEWEST_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2026.1.1),
-    on kwiecień 25, 2026, at 21:34
+    on kwiecień 25, 2026, at 22:48
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
